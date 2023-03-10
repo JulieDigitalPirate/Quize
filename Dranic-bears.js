268adf2ccbfa34d2389ebeb75578805d92bae8c6
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-03-10 16:01:15 +0300

    init fuctread

--- Dranic-bears.js
+++ Dranic-bears.js
@@ -1,1 +1,18 @@
-console.log('hi');
+const fs = require("fs").promises;
+
+const argv = process.argv[2];
+
+const readline = require("readline");
+
+class Themes {
+createPromisesArray(path) {
+let arr = [];
+const read = fs
+.readFile(path, "utf-8", (data, err) => {})
+.then((res) => (arr = res.split("\n")))
+.catch(console.error);
+
+setTimeout(() => 1000);
+
+// console.log(arr);
+}
